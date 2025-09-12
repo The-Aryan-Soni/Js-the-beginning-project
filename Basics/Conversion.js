c2498a39cee70dd24e1dc47dbb89dbc3a2dfcd35
The-Aryan-Soni <158557490+The-Aryan-Soni@users.noreply.github.com>
2025-09-12 20:15:58 +0000

    new file:   .vscode/settings.json 	new file:   Basics/Conversion.js

---
+++ Basics/Conversion.js
@@ -0,0 +1,66 @@
+//If we pass a boolean value
+
+//let score = true
+//console.log(typeof score);
+
+//let valInNum = Number(score);
+//console.log(typeof valInNum);
+//console.log( valInNum);
+
+//  33   -> 33(number)
+//  31ab -> NaN(string)
+//  true -> 1; False -> 0;
+//  Null -> 0(Object)
+//  Undefined ->NaN(Undefined)
+
+
+
+let IsLoggedIn = "2"
+let BoolIsLoggedIn = Boolean(IsLoggedIn)
+//console.log(BoolIsLoggedIn);
+
+// 1 => true
+// "" => false
+// "aryan" => true 
+
+
+//****************OPERATIONS****************
+
+let val = 3
+let negVal = -val
+//console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+
+
+let str1 = 'heck'
+let str2 = 'Day'
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2"+3);
+
+
+console.log(+true);
+console.log(+"");
+
+let gcount = 10
+++gcount
+console.log(gcount);
+   
+// types 
+//primitive-> 7 tpes 
+
+// string,number,boolean,null,undefined,symbol,BigInt
+
+
+
+// Refrence (non primitive)
+// Array, Objects, Functions 
