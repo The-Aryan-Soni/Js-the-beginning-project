8165d594d7e4d08bb5600a739fa42d810e0523b7
The-Aryan-Soni <158557490+The-Aryan-Soni@users.noreply.github.com>
2025-09-28 15:40:18 +0000

    modified:   ControlFlow/truthy.js

--- ControlFlow/truthy.js
+++ ControlFlow/truthy.js
@@ -1,30 +1,32 @@
-const userEmail = []
+// const userEmail = []
 
-if(userEmail){
-    console.log("has Email");
-}
-else {
-    console.log("not an Email");
-}
+// if(userEmail){
+//     console.log("has Email");
+// }
+// else {
+//     console.log("not an Email");
+// }
 
 
 // falsy values => false, 0 ,-0,BigInt 0n,"",null,undefined,NaN
 
 // ṭruthy values => "0",'false'," ",[],{},function(){}->Empty Function
 
-if(userEmail.length === 0){
-    console.log("Array is Empty");
-}
+// if(userEmail.length === 0){
+//     console.log("Array is Empty");
+// }
 
-const empObj = {}
+// const empObj = {}
 
-if (Object.keys(empObj).length === 0){
-    console.log("object is empty");
+// if (Object.keys(empObj).length === 0){
+//     console.log("object is empty");
     
-}
+// }
 
 // nullish coalescing operator (??): null undefined
 
 let val1;
 
-val = 5?? 2
+val = 5 ?? 10
+
+console.log(val1);
